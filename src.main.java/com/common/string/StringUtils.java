5f6b9dd678cbb56a601a2cb875e70b37ac3b8ac2
陈华 <l>
2018-06-06 20:38:07 +0800

    工具类

---
+++ src.main.java/com/common/string/StringUtils.java
@@ -0,0 +1,13 @@
+package com.common.string;
+
+public class StringUtils {
+	
+	/**
+	 * ×Ö·û´®ÊÇ·ñžé¿Õ
+	 * @param check
+	 * @return
+	 */
+	public static boolean isNotBlank(String check) {
+		return false;
+	}
+}
